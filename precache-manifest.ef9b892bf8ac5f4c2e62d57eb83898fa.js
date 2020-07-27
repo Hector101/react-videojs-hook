6f9c1eb300e493e2601ae365a0a6b8eca698840e
Johnson Okoro <hector1014really32@gmail.com>
2020-07-27 15:47:00 +0100

    Updates

---
+++ precache-manifest.ef9b892bf8ac5f4c2e62d57eb83898fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e8cb613a10bd218e0eb1ade3a5b263",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e9b7ff69ce2c084e6bf",
+    "url": "./static/css/2.22fc193c.chunk.css"
+  },
+  {
+    "revision": "2a1697c734ed081e362c",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "5e9b7ff69ce2c084e6bf",
+    "url": "./static/js/2.bf3ed74b.chunk.js"
+  },
+  {
+    "revision": "4eba80e21df2e21401f79434de624c20",
+    "url": "./static/js/2.bf3ed74b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1697c734ed081e362c",
+    "url": "./static/js/main.d273fd29.chunk.js"
+  },
+  {
+    "revision": "432720e463feb4adf3ca",
+    "url": "./static/js/runtime-main.46adcfdf.js"
+  }
+]);
